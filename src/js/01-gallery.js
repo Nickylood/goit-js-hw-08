cdcd012bc7259e49925a52f91edb5389a5721831
Nickylood <110985074+Nickylood@users.noreply.github.com>
2023-03-14 20:18:19 +0100

    finish

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryMarkUp = document.querySelector('.gallery');
+
+const galleryEl = galleryItems.map(({ preview, description, original }) =>
+`<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+<img
+class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"
+/>
+</a>
+</div>`
+).join('');
+
+galleryMarkUp.insertAdjacentHTML('beforeend', galleryEl);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
